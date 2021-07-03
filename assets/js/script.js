ed131b12b2bc40ba909f1a3eaed77a085a0992c1
bowdenshot <anthonyelavezzo@gmail.com>
2021-07-02 20:05:56 -0500

    added change status functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var pageContentEl = document.querySelector('#page-content');
-
 var taskIdCounter = 0;
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do')
-
+var tasksInProgressEl = document.querySelector('#tasks-in-progress');
+var tasksCompletedEl = document.querySelector('#tasks-completed');
 
 var taskFormHandler = function() {
 
@@ -54,7 +54,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
 
     formEl.removeAttribute('data-task-id');
     document.querySelector('#save-task').textContent = 'Add Task';
-    
+
 }
 
 var createTaskEl = function(taskDataObj) {
@@ -170,4 +170,28 @@ var editTask = function(taskId) {
     formEl.setAttribute('data-task-id', taskId);
 
 }
+
+var taskStatusChangeHandler = function(event){
+    // get the task item's id
+    var taskId = event.target.getAttribute('data-task-id');
+
+    // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector('.task-item[data-task-id=\'' + taskId +'\']');
+
+    if (statusValue === 'to do') {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === 'in progress') {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === 'completed') {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 pageContentEl.addEventListener('click', taskButtonHandler);
+pageContentEl.addEventListener('change', taskStatusChangeHandler);
+
